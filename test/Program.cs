d194c541b8e0aa1dcdc69374ee40eab0d7a5e40c
Akib <ali.haider@brainstation-23.com>
2024-04-30 17:40:27 +0600

    Revert "C# Exception Handeling with Try-Catch"
    This reverts commit f63e5abc8c3907207f1a26bae1c7378b6d5da641.

--- test/Program.cs
+++ test/Program.cs
@@ -4,23 +4,7 @@
     {
         static void Main(string[] args)
         {
-
-            try
-            {
-                var numeber = "1234";
-                byte x = Convert.ToByte(numeber);
-                Console.WriteLine(x);
-                Console.WriteLine("Code run without exception!");
-
-
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("The number could not be converted to a byte!");
-            }
-                    
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }

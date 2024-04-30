f63e5abc8c3907207f1a26bae1c7378b6d5da641
Akib <ali.haider@brainstation-23.com>
2024-04-30 17:37:24 +0600

    C# Exception Handeling with Try-Catch

--- test/Program.cs
+++ test/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            try
+            {
+                var numeber = "1234";
+                byte x = Convert.ToByte(numeber);
+                Console.WriteLine(x);
+                Console.WriteLine("Code run without exception!");
+
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("The number could not be converted to a byte!");
+            }
+                    
+
         }
     }
 }

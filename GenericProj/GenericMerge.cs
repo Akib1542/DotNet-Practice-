2cb31698d2975261e5e19263955bddc4c346f706
Akib <ali.haider@brainstation-23.com>
2024-05-02 21:59:59 +0600

    Generic Basic Add!

---
+++ GenericProj/GenericMerge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProj
+{
+    internal class GenericMerge<T> : GenericInterfaceCreate<T> 
+    {
+        public bool isEqual(T val1, T val2)
+        {
+            return val1.Equals(val2);
+        }
+    }
+}
---
+++ GenericProj/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var ab = new GenericMerge<string>();
+
+           
+           Console.WriteLine(ab.isEqual("aa", "bb"));
+            //Console.WriteLine(ab.isEqual(1, 2));
+        }
+    }
+}

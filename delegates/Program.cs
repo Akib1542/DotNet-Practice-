0bb67dfa7c5c69d040ce1d696ce1a59ea0b6d0fa
Akib <ali.haider@brainstation-23.com>
2024-05-06 10:26:21 +0600

    delegate added!

---
+++ delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace delegates
+{
+    internal class Program
+    {
+        delegate void LogDel(string text, DateTime datetime);
+
+        static void Main(string[] args)
+        {
+            LogDel logdel = new LogDel(LogTextToScreen);
+
+            Console.WriteLine("Name: ");
+            var name = Console.ReadLine();
+
+            logdel(name, DateTime.Now);
+            Console.ReadKey();
+        }
+
+         static void LogTextToScreen(string text, DateTime datetime)
+        {
+            Console.WriteLine($"{datetime}: {text}");
+        }
+    }
+}
